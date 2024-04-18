075ab5fd6f23ec19e03beac970a8267755481a2d
Jayalakshmi <10990@intelizign.com>
2024-04-18 22:29:45 +0530

    Description of changes

--- src/components/task-list/task-list-header.tsx
+++ src/components/task-list/task-list-header.tsx
@@ -27,7 +27,7 @@ export const TaskListHeaderDefault: React.FC<{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;Name
+          &nbsp;Namess
         </div>
         <div
           className={styles.ganttTable_HeaderSeparator}
